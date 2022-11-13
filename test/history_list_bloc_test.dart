5153cfc90bc08bcb3f65630d789c66d04929fb9b
oleksa borodie <oleksa.borodie@gmail.com>
2022-11-13 17:19:44 +0200

    history bloc test #130

--- test/history_list_bloc_test.dart
+++ test/history_list_bloc_test.dart
@@ -6,10 +6,10 @@ import 'package:work_tracker/classes/history_model.dart';
 import 'package:work_tracker/classes/work_item.dart';
 import 'package:work_tracker/classes/work_kind.dart';
 import 'dart:convert';
-// import 'package:bloc_test/bloc_test.dart';
-// import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
 
-// import 'package:work_tracker/classes/work_view_model.dart';
+import 'package:work_tracker/classes/work_view_model.dart';
 
 import 'history_model_test.dart';
 
@@ -19,14 +19,14 @@ HistoryListBloc buildBloc() {
   return sut;
 }
 
-// void main() {
-//   test('history bloc items on date', () async {
-//     blocTest<HistoryListBloc, HistoryListState>("loading data on date",
-//         build: buildBloc,
-//         act: (bloc) =>
-//             bloc.add(HistoryLoadListEvent(when: WorkViewModelDumbData.now)),
-//         verify: (_) {
-//           expect(_.state.data.length, 2);
-//         });
-//   });
-// }
+void main() {
+  test('history bloc items on date', () async {
+    blocTest<HistoryListBloc, HistoryListState>("loading data on date",
+        build: buildBloc,
+        act: (bloc) =>
+            bloc.add(HistoryLoadListEvent(when: WorkViewModelDumbData.now)),
+        verify: (_) {
+          expect(_.state.data.length, 2);
+        });
+  });
+}
